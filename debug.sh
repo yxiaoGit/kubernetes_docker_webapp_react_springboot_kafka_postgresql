f21182de34d3dedc77f484a08391e778c4a2c1ab
yxiaoGit <yxabc@yahoo.com>
2026-07-27 17:57:22 -0700

    Add files via upload

---
+++ debug.sh
@@ -0,0 +1,11 @@
+sudo minikube tunnel
+
+kubectl get pods -n ingress-nginx
+
+kubectl get ingress secure-app-gateway
+
+https://localhost     --- click to let browser know to go to the website.
+
+an error pod:
+
+kubectl logs spring-worker-ff5fc56d-4gp68 | more
